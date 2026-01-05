ff59eab8e454d58a00a17c34ff825e82c881fa67
JinJiacheng <Jin_Jiacheng@qq.com>
2026-01-06 00:12:01 +0800

    更新配置

--- testcases/api_test/conftest.py
+++ testcases/api_test/conftest.py
@@ -4,5 +4,6 @@ from testcases.conftest import api_data
 
 @pytest.fixture(scope="function")
 def testcase_data(request):
+    """根据当前测试函数名,自动获取对应的测试数据"""
     testcase_name = request.function.__name__
     return api_data.get(testcase_name)
